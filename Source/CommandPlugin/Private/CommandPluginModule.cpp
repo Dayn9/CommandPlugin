d5b3ae058272319e28e9eea7060b590dc4f8c508
CASSIOPEIA\danem <danemsherman@gmail.com>
2024-05-03 20:25:03 -0500

    defined command plugin log category

--- Source/CommandPlugin/Private/CommandPluginModule.cpp
+++ Source/CommandPlugin/Private/CommandPluginModule.cpp
@@ -4,6 +4,8 @@
 
 #define LOCTEXT_NAMESPACE "FCommandPluginModule"
 
+DEFINE_LOG_CATEGORY(LogCommandPlugin)
+
 void FCommandPluginModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
